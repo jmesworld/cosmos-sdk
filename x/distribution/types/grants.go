8e781497081393f4065ec1d6aa8b301de51016a8
Alex Werner <obusco@gmail.com>
2023-06-07 08:05:44 +0200

    feat(grants): added unmarshalJSON

--- x/distribution/types/grants.go
+++ x/distribution/types/grants.go
@@ -1,13 +1,11 @@
 package types
 
 import (
+	"encoding/json"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"strconv"
 )
 
-type WinningGrantExpiration struct {
-	AtTime   int64 `json:"at_time"`
-	AtHeight int64 `json:"at_height"`
-}
 type WinningGrants []WinningGrant
 
 type WinningGrant struct {
@@ -16,4 +14,69 @@ type WinningGrant struct {
 	ExpireAtHeight sdk.Int        `json:"expire_at_height"`
 	YesRatio       sdk.Dec        `json:"yes_ratio"`
 	ProposalID     sdk.Int        `json:"proposal_id"`
+}
+
+// RawWinningGrant is an intermediate type to help unmarshal JSON
+type RawWinningGrant struct {
+	DAO            string          `json:"dao"`
+	Amount         json.RawMessage `json:"amount"`
+	ExpireAtHeight json.RawMessage `json:"expire_at_height"`
+	YesRatio       string          `json:"yes_ratio"`
+	ProposalID     json.RawMessage `json:"proposal_id"`
+}
+
+func (wg *WinningGrant) UnmarshalJSON(b []byte) error {
+	var raw RawWinningGrant
+	err := json.Unmarshal(b, &raw)
+	if err != nil {
+		return err
+	}
+
+	wg.DAO, err = sdk.AccAddressFromBech32(raw.DAO)
+	if err != nil {
+		return err
+	}
+
+	wg.Amount, err = parseRawInt(raw.Amount)
+	if err != nil {
+		return err
+	}
+
+	wg.ExpireAtHeight, err = parseRawInt(raw.ExpireAtHeight)
+	if err != nil {
+		return err
+	}
+
+	yesRatio, err := sdk.NewDecFromStr(raw.YesRatio)
+	if err != nil {
+		return err
+	}
+	wg.YesRatio = yesRatio
+
+	wg.ProposalID, err = parseRawInt(raw.ProposalID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func parseRawInt(raw json.RawMessage) (sdk.Int, error) {
+	var i int64
+	var s string
+	var err error
+
+	if err = json.Unmarshal(raw, &i); err == nil {
+		return sdk.NewInt(i), nil
+	}
+
+	if err = json.Unmarshal(raw, &s); err != nil {
+		return sdk.Int{}, err
+	}
+
+	if i, err = strconv.ParseInt(s, 10, 64); err != nil {
+		return sdk.Int{}, err
+	}
+
+	return sdk.NewInt(i), nil
 }
